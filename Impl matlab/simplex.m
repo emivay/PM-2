eb905e97114da47f04e85495f3b0bf2fb45847f9
Emilia <emilia.vayssier@hotmail.com>
2018-11-14 01:33:38 +0100

    Ultimos ajustes de hoy

--- Impl matlab/simplex.m
+++ Impl matlab/simplex.m
@@ -1,9 +1,9 @@
 clear all
 
 %Carga de datos del problema y definición de variables
-A = load();
-c = load();
-b = load();
+A = importdata('a2.asc');
+c = importdata('c2.asc');
+b = importdata('b2.asc');
 
 [m,n] = size(A);
 
@@ -11,36 +11,76 @@ b = load();
 
 %Definición de los parámetros para la fase I
 AI = [A eye(m)];
-cI = [zeros(1, n) ones(1, n)];
+cI = [zeros(1, n) ones(1, m)];
 vnI = 1:n;
 vbI = (n+1):(n+m);
 xbI = b;
+zI = cI(vbI)*b; 
 iout= 0;
-bland = true;
+bland = false;
 niter=1;
+
+if bland == true
+display("Inicio del simplex primal con regla de Bland");
+end
+
+if bland == false
+display("Inicio del simplex primal con costos reducidos más negativos");
+end
+
+display("Fase I");
 
 while (iout == 0)
 	fprintf("Iteración número: %4.d", niter)
-	[vb2, vn2, xb2, z2, iout] = simplexP_iter(c, A, b, vb, vn, xb, z, bland);
-	niter++;
+	[vb, vn, xb, z, iout] = simplexP_iter(cI, AI, b, vbI, vnI, xbI, zI, bland);
+	niter = niter + 1;
 end
 
 if iout == 2
+display("Problema ilimitado");
 end
 
 if iout == 3
+display("La solución básica factible es degenerada");
 end
 
 %Si no nos encontramos en ninguno de esos dos casos, iout = 1 y tenemos una solución ópitma
 %Fase II del simplex
 
+if z ~= 0
+	display("Problema infactible");
+end
+
 if z == 0
+	display("Fase II");
+	%Actualización de variables para comenzar la primera iteracion de la fase II
 	iout = 0;
-	z = c(vb)'*xb;
+	xb = xbI;
+	vb = vbI;
+	vn = []
+	%Eliminación de variables artificiales de la fase I
+	for i=1:n
+		if vn2(i) <= n
+			vn = [vn vn2(i)]
+		end
+	end
+	z = c(vb)'*xb;	
 	while (iout == 0)
-	
+		fprintf("Iteración número: %4.d", niter);
+		[vb, vn, xb, z, iout] = simplexP_iter(c, A, b, vb, vn, xb, z, bland);
+		niter = niter + 1;
+	end
+	if iout == 1
+		fprintf("Solución óptima encontrada, iteración %d",niter-1);
+		fprintf("z = %4.f",z );
+	end
+	if iout == 2
+		display("Problema ilimitado");
 	end
 
+	if iout == 3
+		display("La solución básica factible es degenerada");
+	end
 end
 
 
--- Impl matlab/simplexP_iter.m
+++ Impl matlab/simplexP_iter.m
@@ -1,71 +1,73 @@
 %Función que hace una iteración del simplex
 %Tanto p como q son las posiciones en vb y vn
 
-function [vb2, vn2, xb2, z2, iout] = simplexP_iter(c, A, b, vb, vn, xb, z, bland)
+function [vb, vn, xb, z, iout] = simplexP_iter(c, A, b, vb, vn, xb, z, bland)
 
-%Informacion de tamaños
-[m,n] = size(A);
+    %Informacion de tamaños
+    [m,n] = size(A);
 
-%Cálculos auxiliares para costos reducidos
-cn = c(vn);
-cb = c(vb);
-An = A(:, vn);
-B = A(:, vb);
-Bi = inv(B);
+    %Cálculos auxiliares para costos reducidos
+    cn = c(vn);
+    cb = c(vb);
+    An = A(:, vn);
+    B = A(:, vb);
+    Bi = inv(B);
 
-%Cálculo del vector de costos reducidos
-r = cn - cb*Bi*An;
-if (min(r) >= 0) iout = 1; return; end %SBF óptima
+    %Cálculo del vector de costos reducidos
+    r = cn - cb*Bi*An;
 
-%Elección de variable no básica q entrante
+    if (min(r) >= 0) iout = 1; return; end %SBF óptima
 
-if (bland == true)
-[indice, q] = max(vn);
-	for i = 1:n-m
-		if(r(i) < 0)
-			if vn(i) < vn(q)
-			q = i;
-			end
-		end
-	end
+    %Elección de variable no básica q entrante
 
+    if (bland == true)
+    [indice, q] = max(vn);
+        for i = 1:n-m
+            if(r(i) < 0)
+                if vn(i) < vn(q)
+                q = i;
+                end
+            end
+        end
+
+    end
+
+    %Si se utiliza el costo reducido más negativo,
+    %me quedo con el elemento más pequeño del vector de costos reducidos.
+
+    if (bland == false)
+    [costo, q] = min(r);
+    end
+
+    %Cálculo de la DBF de descenso
+    Aq = A(:, vn(q));
+    db = -Bi*Aq;
+    if (min(db) >= 0) iout = 2; return; end %Problema ilimitado
+
+    %Cálculo del paso y selección de la variable básica de salida p
+    for i = 1:m
+        pas = [];
+        if db(i) < 0
+            theta2 = -xb(i)/db(i);
+            pas = [pas theta2];
+            theta = min(pas);
+            if theta2 <= theta
+                p = i;
+                end
+        end
+    end 
+
+    if (theta == 0) iout = 3; return; end %SBF degenerada
+
+    %Actualización y cambios en las bases
+    iout = 0;
+    vb2(p) = vn(q);
+    vn2(q) = vb(p);
+    vb(p) = vb2(p);
+    vn(q) = vn2(q);
+    xb = xb + theta*db;
+    xb(p) = theta;	%ahora en el lugar p se encuentra el valor de x_q
+    z = z + theta*r(q);
+    fprintf("q = %d, rq = %.4f, B(p) = %d,theta* = %.4f,  z = %.4f",vb2(p), r(q), vn2(q), theta, z);
+    fprintf("\n");
 end
-
-%Si se utiliza el costo reducido más negativo,
-%me quedo con el elemento más pequeño del vector de costos reducidos.
-
-if (bland == false)
-[costo, q] = min(r);
-end
-
-%Cálculo de la DBF de descenso
-Aq = A(:, vn(q));
-db = -Bi*Aq;
-if (min(db) >= 0) iout = 2; return; end %Problema ilimitado
-
-%Cálculo del paso y selección de la variable básica de salida p
-for i = 1:m
-	pas = [];
-	if db(i) < 0
-		theta2 = -xb(i)/db(i);
-		pas = [pas theta2];
-		theta = min(pas);
-		if theta2 <= theta
-			p = i;
-			end
-	end
-end 
-
-if (theta == 0) iout = 3; return; end %SBF degenerada
-
-%Actualización y cambios en las bases
-aux = vb(p);
-vb(p) = vn(q);
-vn(q) = aux;
-xb = xb + theta*db;
-xb(p) = theta;	%ahora en el lugar p se encuentra el valor de x_q
-z = z + theta*r(q);
-iout = 0;
-
-fprintf(  
-
